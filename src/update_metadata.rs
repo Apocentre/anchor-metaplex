f7d68e5cecbfd215c8eeccd165a8fcbf1f380caa
ppoliani <ppoliani@gmail.com>
2022-06-23 16:11:26 +0300

    refactor: use DataV2 as param

--- src/update_metadata.rs
+++ src/update_metadata.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program;
 use mpl_token_metadata::{
-  state::{Uses, Creator, Collection, DataV2},
+  state::{DataV2},
   instruction::{
     update_metadata_accounts_v2,
   },
@@ -18,30 +18,16 @@ pub fn update_metadata<'a, 'b, 'c, 'info>(
   accounts: UpdateMetadata<'info>,
   signer_seeds: &[&[&[u8]]],
   new_update_authority: Option<Pubkey>,
-  name: String,
-  symbol: String,
-  uri: String,
-  creators: Option<Vec<Creator>>,
-  seller_fee_basis_points: u16,
+  data: Option<DataV2>,
   primary_sale_happened: Option<bool>,
   is_mutable: Option<bool>,
-  collection: Option<Collection>,
-  uses: Option<Uses>
 ) -> Result<()> {
   let ix = update_metadata_accounts_v2(
     mpl_token_metadata::ID,
     accounts.metadata_account.key(),
     accounts.update_authority.to_account_info().key(),
     new_update_authority,
-    Some(DataV2 {
-      name,
-      symbol,
-      uri,
-      seller_fee_basis_points,
-      creators,
-      collection,
-      uses,
-    }),
+    data,
     primary_sale_happened,
     is_mutable,
   );
