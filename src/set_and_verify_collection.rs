6d2acaa5f46da3508ced1e7fef05f137181cb6ef
ppoliani <ppoliani@gmail.com>
2022-06-23 14:00:18 +0300

    feat: implement the set_and_verify_collection wrapper

--- src/set_and_verify_collection.rs
+++ src/set_and_verify_collection.rs
@@ -2,8 +2,7 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program;
 use anchor_spl::token::{Mint};
 use mpl_token_metadata::{
-  state::{Uses, Creator, Collection},
-  instruction::*,
+  instruction,
 };
 
 #[derive(Accounts)]
@@ -20,7 +19,34 @@ pub struct SetAndVerifyCollection<'info> {
 }
 
 pub fn set_and_verify_collection<'a, 'b, 'c, 'info>(
-  
+  accounts: SetAndVerifyCollection<'info>,
+  signer_seeds: &[&[&[u8]]],
 ) -> Result<()> {
-  todo!()
+  let ix = instruction::set_and_verify_collection(
+    mpl_token_metadata::ID,
+    accounts.metadata_account.key(),
+    accounts.collection_authority.key(),
+    accounts.payer.key(),
+    accounts.update_authority.key(),
+    accounts.collection_mint.key(),
+    accounts.collection_metadata.key(),
+    accounts.collection_master_edition.key(),
+    None,
+  );
+
+  solana_program::program::invoke_signed(
+    &ix,
+    &[
+      accounts.metadata_account,
+      accounts.collection_authority,
+      accounts.payer,
+      accounts.update_authority,
+      accounts.collection_mint.to_account_info(),
+      accounts.collection_metadata,
+      accounts.collection_master_edition,
+      accounts.system_program,
+      accounts.rent,
+    ],
+    signer_seeds,
+  ).map_err(Into::into)  
 }
